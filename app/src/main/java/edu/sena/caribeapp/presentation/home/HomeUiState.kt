ee56627f79bd23f14b916ab355bb30ffc336358e
joseLaptop <josenarvaezcariaga@gmail.com>
2025-06-20 21:08:15 -0500

    feat/Empezando con la presentacion de home Creado HomeUiState

---
+++ app/src/main/java/edu/sena/caribeapp/presentation/home/HomeUiState.kt
@@ -0,0 +1,23 @@
+package edu.sena.caribeapp.presentation.home
+
+import edu.sena.caribeapp.domain.estudiantes.model.ClaseICFES
+import edu.sena.caribeapp.domain.estudiantes.model.Estudiante
+import edu.sena.caribeapp.domain.estudiantes.model.Foro
+import edu.sena.caribeapp.domain.estudiantes.model.Simulacro
+
+/**
+ * Enum que representa las pestañas disponibles en la sección "Mi avance" del HomeScreen.
+ */
+enum class HomeTab {
+    FOROS, SIMULACROS
+}
+
+data class HomeUiState(
+    val isLoading: Boolean = false,
+    val errorMessage: String? = null,
+    val estudiante: Estudiante? = null,
+    val clasesICFES: List<ClaseICFES> = emptyList(),
+    val foros: List<Foro> = emptyList(),
+    val simulacros: List<Simulacro> = emptyList(),
+    val selectedTab: HomeTab = HomeTab.FOROS // Pestaña por defecto
+)
