0ee01314532e08161ad5528972d02ec890730560
joseLaptop <josenarvaezcariaga@gmail.com>
2025-06-21 17:32:03 -0500

    feat/creado classUiState La clase siempre debe estar presente, imposible que sea null los simulacros y foros si pueden ser null

---
+++ app/src/main/java/edu/sena/caribeapp/presentation/clase/ClassUiState.kt
@@ -0,0 +1,13 @@
+package edu.sena.caribeapp.presentation.clase
+
+import edu.sena.caribeapp.domain.estudiantes.model.ClaseICFES
+import edu.sena.caribeapp.domain.estudiantes.model.Foro
+import edu.sena.caribeapp.domain.estudiantes.model.Simulacro
+
+data class ClassUiState(
+    val isLoading: Boolean = false,
+    val errorMessage: String? = null,
+    val clase: ClaseICFES,
+    val foros: List<Foro> = emptyList(),
+    val simulacros: List<Simulacro> = emptyList()
+)
