780dffce1c758cfa8c5c4abcb8a4f578e62c5a13
joseLaptop <josenarvaezcariaga@gmail.com>
2025-06-25 01:58:38 -0500

    feat/UiState de Quiz, osea preguntas, creado

--- app/src/main/java/edu/sena/caribeapp/presentation/quiz/QuizUiState.kt
+++ app/src/main/java/edu/sena/caribeapp/presentation/quiz/QuizUiState.kt
@@ -1,2 +1,18 @@
 package edu.sena.caribeapp.presentation.quiz
 
+import edu.sena.caribeapp.domain.estudiantes.model.ClaseICFES
+import edu.sena.caribeapp.domain.estudiantes.model.Estudiante
+import edu.sena.caribeapp.domain.estudiantes.model.Foro
+import edu.sena.caribeapp.domain.preguntas.model.Pregunta
+import edu.sena.caribeapp.presentation.home.HomeTab
+import edu.sena.caribeapp.presentation.home.SimulacroConClase
+
+data class QuizUiState(
+    val isLoading: Boolean = false,
+    val estudiante: Estudiante? = null,
+    val clasesICFES: List<ClaseICFES>? = emptyList(),
+    val listPreguntas: List<Pregunta>? = emptyList<Pregunta>(),
+    val indexPreguntaActual: Int? = 0,
+    val cantidadPreguntas: Int?  = 0
+    
+)
